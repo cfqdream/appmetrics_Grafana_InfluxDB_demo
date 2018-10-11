0b69329c29f39254a384e78e1c3389d8a7d31538
linkanyway <linkanyway@gmail.com>
2018-10-11 22:53:19 +0800

    init
    init

---
+++ Startup.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using App.Metrics.Configuration;
+using App.Metrics.Extensions.Reporting.InfluxDB;
+using App.Metrics.Extensions.Reporting.InfluxDB.Client;
+using App.Metrics.Reporting.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+        #region 注册 App-Metrics & 配置输出report到influxdb
+
+            var database = "MyMetrics";
+            var uri = new Uri(" http://127.0.0.1:8086"); //本地Docker中运行的influx实例
+            services.AddMetrics(options =>
+                {
+                    options.WithGlobalTags((globalTags, info) =>
+                    {
+                        globalTags.Add("app", info.EntryAssemblyName);
+                        globalTags.Add("env", "stage");
+                    });
+                })
+                .AddHealthChecks()
+                .AddReporting(
+                    factory =>
+                    {
+                        factory.AddInfluxDb(
+                            new InfluxDBReporterSettings
+                            {
+                                InfluxDbSettings = new InfluxDBSettings(database, uri),
+                                ReportInterval = TimeSpan.FromSeconds(5)
+                            });
+                    })
+                .AddMetricsMiddleware(options => options.IgnoredHttpStatusCodes = new[] {404});
+
+        #endregion
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+            //添加Metric Filter到mvc
+            services.AddMvc(options => options.AddMetricsResourceFilter())
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory,
+            IApplicationLifetime lifetime)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            loggerFactory.AddConsole(Configuration.GetSection(" Logging "));
+            app.UseMetrics();
+            app.UseMetricsReporting(lifetime);
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
